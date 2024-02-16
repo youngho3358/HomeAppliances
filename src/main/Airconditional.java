a8be91502c11c4f23901d3fccd6b014ef1845eed
won-jae-jang <dnjswo410@naver.com>
2024-02-16 14:33:58 +0900

    냉장고 온도 기능 추가

--- src/main/Airconditional.java
+++ src/main/Airconditional.java
@@ -2,6 +2,8 @@ package main;
 
 public class Airconditional implements HomeAppliances{
 
+    private int temperature;
+
     @Override
     public void turnOn() {
         System.out.println("냉장고를 킵니다.");
@@ -10,5 +12,21 @@ public class Airconditional implements HomeAppliances{
     @Override
     public void turnOff() {
         System.out.println("냉장고를 끕니다.");
+    }
+
+    public void upTemp(int raiseNum) {
+        temperature += raiseNum;
+    }
+
+    public void downTemp(int lowerNum) {
+        temperature -= lowerNum;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
     }
 }
